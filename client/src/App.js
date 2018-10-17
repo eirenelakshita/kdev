13dedfd86b91af96d9594e52a663e6e754bca723
victoriaashling <victoria.ashling@gmail.com>
2018-10-17 13:30:44 -0400

    form initial json

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import HealthDataForm from "./pages/HealthDataForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>Hello World.</header>
+        <HealthDataForm />
       </div>
     );
   }
---
+++ client/src/pages/Calendar.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from "react";
+import moment from "moment";
+import { Container, Row, Col } from "../components/Grid";
+
+class Calendar extends PureComponent {
+  state = {
+    moment: ""
+  }
+
+  componentDidMount() {
+    this.getMonthYear();
+  }
+
+  getMonthYear = () => {
+    this.setState({ moment: moment().format("MMMM YYYY") })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <span>{this.state.moment}</span>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default Calendar;
