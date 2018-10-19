01a320c2aa57690c26c053f53f49e5872d59997b
victoriaashling <victoria.ashling@gmail.com>
2018-10-18 21:15:16 -0400

    added modular react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PatientDataForm from "./pages/Forms";
-import Messages from "./pages/Messasges";
+import PatientRouter from "./Routes/PatientRouter.js";
+import DoctorRouter from "./Routes/DoctorRouter.js";
 import Home from "./pages/Home";
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/messages" component={Messages} />
-      <Route path="/user-profile-info" component={PatientDataForm} />
-    </Switch>
-  </Router>
-)
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            {/* The following route will be for the landing / login page once Kevin is ready to add it. */}
+            {/* <Route exact path="/" component={LoginPage} /> */}
+            <Route path="/patients" render={props => <PatientRouter />} />
+            <Route path="/doctors" render={props => <DoctorRouter />} />
+            <Route component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
 
 export default App;
 
