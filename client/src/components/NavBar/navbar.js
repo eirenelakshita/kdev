dbfb590b72ff9391ba8226f79a73453d65c0b743
daydoug14 <daydoug14@gmail.com>
2018-10-18 21:18:45 -0400

    Navbar

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -7,18 +7,19 @@ const navbar = () => (
    <div className="row">
       <div className="col-sm-3" style={{backgroundColor:"clear"}}></div>
       <div className="col-sm-6 center" style={{backgroundColor:"clear"}}>
-        <a href="#" className="button">Link Button</a>
+        <a href="#" className="button">Primary Care</a>
       </div>
       <div className="col-sm-3" style={{backgroundColor:"clear"}}></div>
     </div>
 
     <div className="row">
       <div className="col-sm-4 right" style={{backgroundColor:"clear"}}>
-            <a href="#" className="button">Link Button</a></div>
-      <div className="col-sm-4" style={{backgroundColor:"clear"}}></div>
+            <a href="#" className="button">Dentist</a></div>
+      <div className="col-sm-4 center" style={{backgroundColor:"clear"}}>
+      </div>
       
       <div className="col-sm-4 left" style={{backgroundColor:"clear"}}>
-            <a href="#" className="button">Link Button</a></div>
+            <a href="#" className="button">Optometry</a></div>
     </div>
       
 
@@ -26,33 +27,33 @@ const navbar = () => (
 
       <div className="row">
           <div className="col-sm-3 right" style={{backgroundColor:"clear"}}>
-            <a href="#" className="button">Link Button</a></div>
+            <a href="#" className="button">Male/Female</a></div>
           <div className="col-sm-3" style={{backgroundColor:"clear"}}></div>
           <div className="col-sm-3 center" style={{backgroundColor:"clear"}}>
           </div>
           <div className="col-sm-3 left" style={{backgroundColor:"clear"}}>
-            <a href="#" className="button">Link Button</a></div>
+            <a href="#" className="button">Dermatologist</a></div>
       </div>
         
 
 
       <div className="row">
         <div className="col-sm-2 right" style={{backgroundColor:"clear"}}>
-           <a href="#" className="button">Link Button</a></div>
+           <a href="#" className="button">Pharmacy</a></div>
           <div className="col-sm-2" style={{backgroundColor:"clear"}}></div>
           <div className="col-sm-2 center" style={{backgroundColor:"clear"}}>
-           </div>
+          </div>
           <div className="col-sm-2 left" style={{backgroundColor:"clear"}}>
           </div>
            <div className="col-sm-2 center" style={{backgroundColor:"clear"}}>
            </div>
            <div className="col-sm-2 left" style={{backgroundColor:"clear"}}>
-           <a href="#" className="button">Link Button</a></div>
+           <a href="#" className="button">Cardiologist</a></div>
 
            </div>
 
-           <h1>Hello World!</h1>
-    <p>Working on a circular NavBar.</p>
+           <h1>⚕️   My Patient Portal   ⚕️</h1>
+    <p>Navigate Further From This Page</p>
    
   </div>
 );
