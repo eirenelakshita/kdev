9bd1d607f98ee3979fb5894ff6934a21251ef1c3
victoriaashling <victoria.ashling@gmail.com>
2018-10-23 18:57:27 -0400

    doing some cleanup

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,27 +2,26 @@ import axios from "axios";
 
 
 export default {
+
   postMessage: function(input) {
     return axios.post("/api/messages", input);
   },
+
   getRx: function() {
     return axios.get("/api/rx");
   },
-   // Gets all visits
-   getVisits: function() {
+
+  getVisits: function() {
     return axios.get("/api/visits");
   },
-
-  // Gets the book with the given id
   getVisit: function(id) {
     return axios.get("/api/visits/" + id);
   },
-  // Deletes the book with the given id
   deleteVisit: function(id) {
     return axios.delete("/api/visits/" + id);
   },
-  // Saves a book to the database
   saveVisit: function(visitData) {
     return axios.post("/api/visits", visitData);
   }
+  
 };
--- client/src/utils/FormAPI.js
+++ client/src/utils/FormAPI.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default {
   submitForm: function(data) {
     console.log("inside api file");
-    // return axios.post("/api/identifyinginfo", data);
+    return axios.post("/api/identifyinginfo", data);
   }
 };
