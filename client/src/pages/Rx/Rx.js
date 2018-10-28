1c932edb1b35e6a3a427bb1db621ecee8d6b3973
daydoug14 <daydoug14@gmail.com>
2018-10-27 21:31:55 -0400

    cleaning

--- client/src/pages/Rx/Rx.js
+++ client/src/pages/Rx/Rx.js
@@ -71,8 +71,8 @@ class Rx extends Component {
                       <Row>
                         <Col size="3" />
                         <Col size="9">
-                          <p>Notes: {rx.drugInfo}</p>
-                          {/* <p>Prescription: {rx.Prescription}</p> */}
+                          <p>Notes: {rx.rxInfo}</p>
+                          
                         </Col>
                       </Row>
                       <DeleteBtn onClick={() => this.deleteRx(rx._id)} />
